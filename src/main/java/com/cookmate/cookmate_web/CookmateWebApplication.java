0ece18e6fdfce1d3aaf26fa8b47f94b716888ca5
[개발자]강보람 <kb6408@naver.com>
2026-01-18 14:13:24 +0900

    [CNF] 의존성 추가 및 JPA Auditing 어노테이션 추가   - Spring-Validation 의존성 추가   - 생성일, 수정일 기록을 위한 어노테이션 추가

--- src/main/java/com/cookmate/cookmate_web/CookmateWebApplication.java
+++ src/main/java/com/cookmate/cookmate_web/CookmateWebApplication.java
@@ -2,7 +2,9 @@ package com.cookmate.cookmate_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CookmateWebApplication {
 
